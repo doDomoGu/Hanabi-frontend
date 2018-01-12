702b462f91e85514b9882f3338738da3ea7fc744
doDomoGu <kaka_71936410@qq.com>
2018-01-12 21:11:04 +0800

    遵循vue的eslint 对代码进行整理

--- static/js/index/index.js
+++ static/js/index/index.js
@@ -5,61 +5,56 @@ export default {
     return {
     }
   },
-  mounted: function(){
-
-
+  mounted: function () {
   },
-  created: function(){
-    this.$store.dispatch('common/SetTitle2','Hanabi');
-    if(this.isLogin()){
-      this.$store.dispatch('common/SetTitle2','('+this.$store.getters['auth/user_id']+')');
-      this.$store.dispatch('room/List');
-      this.$store.dispatch('my_room/GetInfo',{mode:'simple',force:true});
-      this.$store.dispatch('my_game/GetInfo',{mode:'simple',force:true});
+  created: function () {
+    this.$store.dispatch('common/SetTitle2', 'Hanabi')
+    if (this.isLogin()) {
+      this.$store.dispatch('common/SetTitle2', '(' + this.$store.getters['auth/user_id'] + ')')
+      this.$store.dispatch('room/List')
+      this.$store.dispatch('my_room/GetInfo', { mode: 'simple', force: true })
+      this.$store.dispatch('my_game/GetInfo', { mode: 'simple', force: true })
     }
   },
-  computed : {
-    room_list : function() {
-      let room_list = this.$store.getters['room/list'];
+  computed: {
+    roomList: function () {
+      const list = this.$store.getters['room/list']
+      for (const l of list) {
+        l._title = '<mt-badge size="small">' + l.id + '</mt-badge>' + ' ' + l.title
+        l._title = (l.id < 100 ? l.id < 10 ? '00' + l.id : '0' + l.id : l.id) + ' ' + l.title
 
-      for(let room of room_list){
-        room._title = '<mt-badge size="small">'+room.id+'</mt-badge>'+' '+room.title;
-        room._title = (room.id<100?room.id<10?'00'+room.id:'0'+room.id:room.id)+' '+room.title;
-
-        if(room.password!==''){
-          room._title += '[lock]';
+        if (l.password !== '') {
+          l._title += '[lock]'
         }
       }
-      return room_list;
+      return list
     }
   },
   methods: {
-    toLogin(){
-      this.$router.push({path:'/login'});
+    toLogin () {
+      this.$router.push({ path: '/login' })
     },
-    /*toRegister(){
+    /* toRegister(){
 
     },*/
-    isLogin(){
-      return this.$store.getters['auth/is_login'];
+    isLogin () {
+      return this.$store.getters['auth/is_login']
     },
-    enterRoom(room_id){
-      let that = this;
-      this.$store.dispatch('my_room/Enter',room_id).then((res)=>{
-        if(res.success){
-          that.$router.push('/room');
-        }else {
-          MessageBox.alert(res.msg + '(' + room_id + ')').then(action => {
-            //console.log(action);
-          });
+    enterRoom (roomId) {
+      const that = this
+      this.$store.dispatch('my_room/Enter', roomId).then((res) => {
+        if (res.success) {
+          that.$router.push('/room')
+        } else {
+          MessageBox.alert(res.msg + '(' + roomId + ')')
         }
       })
     },
-    isInRoom(){
-      return this.$store.getters['my_room/room_id']>0;
+    isInRoom () {
+      return this.$store.getters['my_room/room_id'] > 0
     },
-    isInGame(){
-      return this.$store.getters['my_game/is_playing'];
+    isInGame () {
+      return this.$store.getters['my_game/is_playing']
     }
   }
 }
