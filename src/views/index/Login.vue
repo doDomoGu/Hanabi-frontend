c909d91edf6bee1c58409483b1ada904a42a8277
doDomoGu <kaka_71936410@qq.com>
2018-01-14 11:58:35 +0800

    登录页面也昂是

--- src/views/index/Login.vue
+++ src/views/index/Login.vue
@@ -1,10 +1,11 @@
 <template>
-    <section>
+    <div id="login">
         <mt-field label="用户名" placeholder="请输入用户名" @keydown.enter.native="onSubmit" v-model="form.username"></mt-field>
 
         <mt-field label="密码" placeholder="请输入密码" type="password" v-model="form.password" @keydown.enter.native="onSubmit" ></mt-field>
 
-        <mt-button type="default" size="large" @click.native="onSubmit">submit</mt-button>
-    </section>
+        <mt-button id="loginBtn" type="default" size="large" @click.native="onSubmit">登录</mt-button>
+    </div>
 </template>
 <script src="@js/index/login.js"></script>
+<style src="@css/index/login.scss"></style>
